3d92aec6bab34b4d4f3e12ead84b6b5a1db4ebde
Diegonesi <diego.a.pineda.v@gmail.com>
2025-05-14 12:22:10 -0400

    Array String visto en la ayudantia

--- String y char.cpp
+++ String y char.cpp
@@ -14,17 +14,17 @@ int main(){
     string Primer_String = Texto;
     cout<<Primer_String << " | " << Primer_String[0]<<" | Tamaño con String: "<<Primer_String.size()<<" | "<<Primer_String[23]<<endl; // Asi mismo si haces esto puedes ver el caracter en la posicion n del string
     // IMPORTANTE: RECORDAR USAR .size() para STRINGS y para Bool/Int/Double/Float usar sizeof(arr) / sizeof(arr[0]);
-    
-    // Vemos como en el string o char existe mas tamaño que el definido, esto llama a caracteres de la tabla ASCII que estan en una direccion de memoria. (Puede ser "Vacio")
 
-    for(int i = Primer_String.size()-1; i>=0;i--){ // Recorrer de manera inversa
-        cout<<Primer_String[i]<<endl;
-        if(Primer_String[i]=='A'){
-            cout<<"Entre aqui"<<endl;
+    // Vemos como en el string o char existe mas tamaño que el definido, esto llama a caracteres de la tabla ASCII que estan en una direccion de memoria. (Puede ser "Vacio")
+    
+    for(int i = 0; i<Primer_String.size();i++){ // Recorrer de manera inversaun String
+            cout<<Primer_String[i]<<endl;
+            if(Primer_String[i]=='A'){
+                cout<<"Entre aqui"<<endl;
+            }
         }
-    }
     cout<<string(100,'=')<<endl;
-    for(int i = 0; i<Primer_String.size();i++){ // Recorrer de manera inversa
+    for(int i = Primer_String.size()-1; i>=0;i--){ // Recorrer un String de manera inversa
         cout<<Primer_String[i]<<endl;
         if(Primer_String[i]=='A'){
             cout<<"Entre aqui"<<endl;
